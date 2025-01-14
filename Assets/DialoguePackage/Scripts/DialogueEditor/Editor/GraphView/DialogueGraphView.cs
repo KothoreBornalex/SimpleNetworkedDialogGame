24990e62bd8f1d160fe934ee2cc297a3512b5cce
Brach <alessandrotano@outlook.fr>
2025-01-14 16:48:53 +0100

    Base Window & GraphView

---
+++ Assets/DialoguePackage/Scripts/DialogueEditor/Editor/GraphView/DialogueGraphView.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DialogueGraphView : GraphView
+{
+    private string styleSheetsName = "GraphViewStyleSheet";
+    private DialogueEditorWindow dialogueEditorWindow;
+
+    public DialogueGraphView(DialogueEditorWindow _editorWindow) 
+    {
+        dialogueEditorWindow = _editorWindow;
+
+        StyleSheet tmpStyleSheet = Resources.Load<StyleSheet>(styleSheetsName);
+        styleSheets.Add(tmpStyleSheet);
+
+        SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+        this.AddManipulator(new FreehandSelector());
+
+        GridBackground grid = new GridBackground();
+        Insert(0, grid);
+
+        grid.StretchToParentSize();
+    }
+}
---
+++ Assets/DialoguePackage/Scripts/DialogueEditor/Runtime/DialogueContainerSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue/New Dialogue")]
+[System.Serializable]
+public class DialogueContainerSO : ScriptableObject
+{
+
+}
